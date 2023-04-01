14f7cb5e87948701bfc734f3a0aae06b4dc60cb1
gnurullayev <nurullayevgayrat73@gmail.com>
2023-04-01 22:12:00 +0500

    change footer component

--- src/components/layout/components/footer/Footer.js
+++ src/components/layout/components/footer/Footer.js
@@ -2,7 +2,37 @@ import React from "react";
 import styles from "./footer.module.scss";
 
 const Footer = () => {
-  return <div className={styles.footer}>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <div className={`${styles.footer__container} container`}>
+        <div className={styles.footer__inner}>
+          <a href="/" className={styles.footer__logo}>
+            <img src="" alt="" className={styles.logo} />
+          </a>
+
+          <ul className={styles.footer__list}>
+            <li className={styles.footer__item}>
+              <a href="/" className={styles.footer__link}>
+                Block
+              </a>
+            </li>
+
+            <li className={styles.footer__item}>
+              <a href="/" className={styles.footer__link}>
+                About
+              </a>
+            </li>
+
+            <li className={styles.footer__item}>
+              <a href="/" className={styles.footer__link}>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
